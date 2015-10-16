e76e723bf13593d69ccbc3ed61080f3a6dc5b8dd
samarthapple1409 <samarthapple1409@gmail.com>
2015-10-16 18:23:51 +0000

    Finish toy app

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  resources :microposts
   resources :users
   root 'application#hello'
   # The priority is based upon order of creation: first created -> highest priority.
